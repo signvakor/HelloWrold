e8051bd4564c7a291907cccc0cc809af55edc11c
signvakor <vakor@vakordeMacBook-Pro.local>
2015-04-12 10:38:05 +0800

    Revert "test"
    This reverts commit 1f2388602bed1579b30954935f41f813f1053cdb.

--- numb/numb/AppDelegate.m
+++ numb/numb/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    UIWindow *window = [[UIWindow alloc] sssssinitWithFrame:[UIScreen mainScreen].bounds];
+    UIWindow *window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     
     ViewController *vc = [[ViewController alloc] init];
     UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
